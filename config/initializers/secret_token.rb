9dce45988aa77469a6bf623d6670d032d964902b
Gerrit Wanderer <wanderwelten@googlemail.com>
2011-01-03 16:44:00 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nancy::Application.config.secret_token = '8899ca0fa8ede1c915852be1bd512412e7cf292b05ecc626767803f7b35449f2cb6082f6cff67938345cd445682c38d4854b84c5a530af1e17380dfd0b182af2'
