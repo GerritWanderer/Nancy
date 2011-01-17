034bc782cc8d66bd722be9fb4a4ea1e09be8720f
Gerrit Wanderer <wanderwelten@googlemail.com>
2011-01-17 18:26:14 +0100

    Set User-Layout within application controller / Style Login/Registration-Pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  layout :layout_by_resource
+  def layout_by_resource
+    if devise_controller?
+      "users"
+    else
+      "application"
+    end
+  end
 end
