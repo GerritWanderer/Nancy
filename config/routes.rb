29612aa006b1b79cb10a890fb829f67448e37b18
Gerrit Wanderer <wanderwelten@googlemail.com>
2011-01-17 17:35:44 +0100

    Change default devise routes to /login, /registration and /logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Nancy::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :skip => [:registration, :sessions] do
+    get '/login' => 'devise/sessions#new', :as => :new_user_session
+    post '/login' => 'devise/sessions#create', :as => :user_session
+    get '/registration' => 'devise/registrations#new', :as => :new_user_registration
+    post '/registration' => 'devise/registrations#create', :as => :user_registration
+    get '/logout' => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
+  
   resources :works
   resources :customers do
     resources :locations do
