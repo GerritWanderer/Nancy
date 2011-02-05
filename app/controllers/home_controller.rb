f42ed09a9d7a4e2a04e6127df8d8f2410c4e364d
Gerrit Wanderer <wanderwelten@googlemail.com>
2011-02-05 20:39:36 +0100

    Fix flash-key assignment for heroku

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     flash.each do |key, value|
-      flash[key.intern] = value
+      flash[key] = value
     end
     redirect_to user_signed_in? == true ? works_path : new_user_session_path
   end
